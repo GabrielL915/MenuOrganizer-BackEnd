70748a6d2b87902f45efefafba061b0fd405b23d
gabriel gomes <gabrielluizgomes1@gmail.com>
2024-03-19 10:56:47 -0300

    implementacao triggers de criar meal

--- migrations/20240301200931_user.js
+++ migrations/20240301200931_user.js
@@ -5,9 +5,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary().defaultTo(knex.fn.uuid());
-    /*   table.string('username').notNullable().unique();
-    table.string('password').notNullable();
-    table.string('email').notNullable().unique(); */
     table.timestamp('createdAt').defaultTo(knex.fn.now());
     table.timestamp('updatedAt').defaultTo(knex.fn.now());
   });
